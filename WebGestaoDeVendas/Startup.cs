a472113198015d9f2ba5ae18ce85468b4cb4e0d4
ThiagoPaiva89 <thiagoinaciopaiva@hotmail.com>
2022-01-08 18:05:43 -0300

    CRUD Scaffolding

--- WebGestaoDeVendas/Startup.cs
+++ WebGestaoDeVendas/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebGestaoDeVendas.Data;
 
 namespace WebGestaoDeVendas
 {
@@ -33,6 +35,9 @@ namespace WebGestaoDeVendas
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebGestaoDeVendasContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebGestaoDeVendasContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
