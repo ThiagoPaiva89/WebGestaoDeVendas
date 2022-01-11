09d2e8343ed2e35d0353d6ab4cc7f22fd660b514
ThiagoPaiva89 <thiagoinaciopaiva@hotmail.com>
2022-01-11 10:00:12 -0300

    Servicos de Vendedores e Listagem de todos

---
+++ WebGestaoDeVendas/Services/VendedorService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebGestaoDeVendas.Models;
+using WebGestaoDeVendas.Data;
+
+namespace WebGestaoDeVendas.Services
+{
+    public class VendedorService
+    {
+        private readonly WebGestaoDeVendasContext _context;
+
+        public VendedorService(WebGestaoDeVendasContext context)
+        {
+            _context = context;
+        }
+
+        public List<Vendedor> FindAll()
+        {
+            return _context.Vendedor.ToList();
+        }
+    }
+}
