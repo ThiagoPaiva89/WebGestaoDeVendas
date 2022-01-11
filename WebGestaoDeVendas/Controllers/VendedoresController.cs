b486c114dc18d8b67eb1c720cca6dfd14af9329e
ThiagoPaiva89 <thiagoinaciopaiva@hotmail.com>
2022-01-11 10:35:20 -0300

    Formulario de Cadastro de Vendedores - Simples

--- WebGestaoDeVendas/Controllers/VendedoresController.cs
+++ WebGestaoDeVendas/Controllers/VendedoresController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebGestaoDeVendas.Services;
+using WebGestaoDeVendas.Models;
 
 namespace WebGestaoDeVendas.Controllers
 {
@@ -24,5 +25,19 @@ namespace WebGestaoDeVendas.Controllers
 
             return View(lista);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Vendedor vendedor)
+        {
+            _vendedorService.Insert(vendedor);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
